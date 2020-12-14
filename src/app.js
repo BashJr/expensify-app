92f097a2d478c6e6b62c559b93406b7fdfd66276
Zach Watts <zacharymwatts@gmail.com>
2020-12-13 19:00:30 -0600

    Able to fetch data from DBs

--- src/app.js
+++ src/app.js
@@ -3,13 +3,12 @@ import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import 'react-dates/initialize';
 import './firebase/firebase';
 
 const store = configureStore();
@@ -19,4 +18,9 @@ const jsx = (
     </Provider>
 );
 
-ReactDom.render(jsx, document.getElementById('app'));
+ReactDom.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDom.render(jsx, document.getElementById('app'));
+});
+
