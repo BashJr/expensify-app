59dfb9538e947b34457541a06bb8e746b1595f45
Zach Watts <zacharymwatts@gmail.com>
2021-01-06 09:53:19 -0600

    Added styling

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 const jsx = (
@@ -26,7 +27,7 @@ const renderApp = () => {
     }
 };
 
-ReactDom.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDom.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
